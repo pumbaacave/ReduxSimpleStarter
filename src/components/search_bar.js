1b733549170c0460a8f9a59ef4c8d5fcd864ab47
若帆 叶 <ruofan.ye@gu3.co.jp>
2018-10-01 19:07:39 +0900

    implement detail_list_item

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { runInThisContext } from 'vm';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: 'starting value'};
+    }
+    render() {
+         return (
+             <div>
+                <input 
+                value={this.state.term}
+                onChange={event => this.setState({term: event.target.value})} 
+                />
+                <br></br>
+                Value of the input: {this.state.term}
+            </div>
+         ); 
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyB-fHFQ6wIFDhScRisLBAxj4CzouOTaUug'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// A component should produce some HTML 
+class App extends Component {
+    constructor(props){
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+            );
+    } 
+}
+
+//  Take this components generated HTML and put it 
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
