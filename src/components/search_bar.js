4ab4a5bc9e4c6de564bf3e2baaf17e9d2df887ff
若帆 叶 <ruofan.ye@gu3.co.jp>
2018-10-02 11:37:03 +0900

    make searchbar interactivew

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,12 +12,17 @@ class SearchBar extends Component {
              <div className="search-bar">
                 <input 
                 value={this.state.term}
-                onChange={event => this.setState({term: event.target.value})} 
+                onChange={event => this.onInputChange(event.target.value)} 
                 />
                 <br></br>
                 Value of the input: {this.state.term}
             </div>
          ); 
+    }
+       
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
