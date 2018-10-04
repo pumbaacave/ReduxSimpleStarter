45f6951a3cad562d482bed6545325ce95f105da8
若帆 叶 <ruofan.ye@gu3.co.jp>
2018-10-04 19:05:20 +0900

    add switch in bwouserRouter

---
+++ src/components/posts_new.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PostNew extends Component {
+  render() {
+    return <div>make new post!</div>;
+  }
+}
+
+export default PostNew;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import promise from "redux-promise";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import reducers from "./reducers";
 import PostsIndex from "./components/posts_index";
+import PostNew from "./components/posts_new";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,7 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
